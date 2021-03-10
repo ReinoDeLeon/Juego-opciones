bb3087b8b35c785076e74e2dcf612c573cf4ccdb
Alex Ruiz <alexruizvkramirez@gmail.com>
2021-03-10 10:30:41 +0100

    Imprimir Hola

--- src/JuegoOpciones.java
+++ src/JuegoOpciones.java
@@ -4,10 +4,7 @@ public class JuegoOpciones {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//probando commit
-
-		//prueba origin
-		
+		System.out.println("Hola");
 	}
 
 }
